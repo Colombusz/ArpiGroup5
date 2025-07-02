4dea98191478d9fd7b4cf1a14eb1974fbdbc0e1f
eli <eliseo.lim12@gmail.com>
2025-07-02 10:57:13 -0700

    changed

--- MQTTCOM.py
+++ MQTTCOM.py
@@ -1,29 +1,16 @@
-import paho.mqtt.client as mqtt
-
-BROKER = "localhost"
-SUB_TOPIC = "esp32/test"
-PUB_TOPIC = "esp32/control"
-CLIENT_ID = "RaspberryPiClient"
-
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print("✅ Connected to MQTT broker")
-        client.subscribe(SUB_TOPIC)
-        print(f"📡 Subscribed to: {SUB_TOPIC}")
-    else:
-        print(f"❌ Failed to connect, return code {rc}")
-
 def on_message(client, userdata, msg):
     message = msg.payload.decode()
     print(f"📨 Message from {msg.topic}: {message}")
 
-    # Respond back to ESP32
-    response = "KINGINA MO"
-    client.publish(PUB_TOPIC, response)
-    print(f"📤 Replied with: {'KINGINA MO'}")
+    if msg.topic == "esp32/test/esp1":
+        print("🔵 Message received from ESP1")
+        response = "Hello ESP32-1"
+    elif msg.topic == "esp32/test/esp2":
+        print("🟢 Message received from ESP2")
+        response = "Hello ESP32-2"
+    else:
+        print("⚠️ Message from unknown ESP")
+        response = "Hello Unknown ESP32"
 
-client = mqtt.Client(CLIENT_ID)
-client.on_connect = on_connect
-client.on_message = on_message
-client.connect(BROKER, 1883, 60)
-client.loop_forever()
+    client.publish("esp32/control", response)
+    print(f"📤 Replied with: {response}")
